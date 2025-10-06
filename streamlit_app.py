aebbcbdc004860af4be454b17ed3e25849d87f61
Kalyani121297 <ktirpude@nicesoftwaresolutions.com>
2025-10-06 15:35:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,8 @@ session = cnx.session()
 
 # Fetch available fruits
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'),col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 ingredients_list = st.multiselect('Choose up to 5 Ingredients:', my_dataframe, max_selections=5)
 st.write(ingredients_list)
 st.text(ingredients_list)
